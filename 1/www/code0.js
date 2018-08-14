15a90314f875fb518d1186089188da89052859ce
SpectrumStudios <twohumansproductions@gmail.com>
2018-08-14 10:22:41 -0400

    Add files

---
+++ 1/www/code0.js
@@ -0,0 +1,17 @@
+gdjs.TestCode = {};
+
+gdjs.TestCode.conditionTrue_0 = {val:false};
+gdjs.TestCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.TestCode.eventsList0xaf630 = function(runtimeScene, context) {
+
+}; //End of gdjs.TestCode.eventsList0xaf630
+
+
+gdjs.TestCode.func = function(runtimeScene, context) {
+context.startNewFrame();
+
+gdjs.TestCode.eventsList0xaf630(runtimeScene, context);return;
+}
+gdjs['TestCode']= gdjs.TestCode;
